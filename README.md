52abe3be7d37837588f7caeabbd2dae7dad7c6a2
khaled <khvb6@yahoo.com>
2023-09-12 10:05:45 +0200

    - Added web application for testing.
    - Added workflow to build .net core app.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # test-actions
 Testing github actions
+
+# Testing .net 7 build
+Test building .net 7 web app by actions.
---
+++ src/WebApi/Program.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+WebApplication app
+    = WebApplication
+        .CreateBuilder(args)
+        .Build();
+
+app.MapGet("/", async context
+    => await context
+        .Response
+            .WriteAsync("Hello Github!")
+            .ConfigureAwait(false));
+
+app.Run();
